aee7af6652b0b007da395d469b6feb3e7baa596b
Vasko Zdravevski <vzdravevski@gmail.com>
2017-05-01 22:27:19 -0600

    greedy easy - beautiful pairs

---
+++ beautiful_pairs/beautiful_pairs_test.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bytes"
+	"strings"
+	"testing"
+)
+
+var testcases = []struct {
+	in, out string
+}{
+	{"3 1 2 2 1 2 3", "3\n"},
+}
+
+func TestBeautifulPairs(t *testing.T) {
+	for _, c := range testcases {
+		stdin = strings.NewReader(c.in)
+		buff := new(bytes.Buffer)
+		stdout = buff
+		main()
+		out := buff.String()
+		if out != c.out {
+			t.Errorf("%v does not equal %v", out, c.out)
+		}
+	}
+}
+
+func BenchmarkBeautifulPairs(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, c := range testcases {
+			stdin = strings.NewReader(c.in)
+			buff := new(bytes.Buffer)
+			stdout = buff
+			main()
+		}
+	}
+}
